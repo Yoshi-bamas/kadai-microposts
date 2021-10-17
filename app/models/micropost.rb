32219899c72ed4c672d937156a9c6abe510cbb9c
yoshiya.oba <yoshirinpana@gmail.com>
2021-10-17 08:43:27 +0000

    heroku deploy4

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,13 +4,5 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   has_many :favorites
   
-  def like(micropost)
-    self.favorites.find_or_create_by(micropost_id: micropost.id)
-  end
-
-  def unlike(micropost)
-    relationship = self.favorites.find_by(micropost_id: micropost.id)
-    relationship.destroy if relationship
-  end
 
 end
